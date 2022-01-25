15983b24304377418b2a302a668201437787a470
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-25 13:46:24 -0300

    structura de funcion delete usuario agregada

--- src/main/java/com/cliente/dr/controllers/UsuarioController.java
+++ src/main/java/com/cliente/dr/controllers/UsuarioController.java
@@ -4,7 +4,9 @@ package com.cliente.dr.controllers;
 import com.cliente.dr.dao.UsuarioDao;
 import com.cliente.dr.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,9 +16,14 @@ public class UsuarioController {
     @Autowired
     private UsuarioDao usuarioDao;
 
-    @RequestMapping(value = "usuarios")
+    @RequestMapping(value = "api/usuarios")
     public List<Usuario> getUsuarios(){
         return usuarioDao.getUsuarios();
+    }
+
+    @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
+    public void deleteUsuarios(@PathVariable long id){
+
     }
 
 
