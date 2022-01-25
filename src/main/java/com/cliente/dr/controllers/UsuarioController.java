f762f066001d5fa8e3ce5735a28e2264513aa82e
DiegoRodriguez-sc <diego.rodriguez.sc@gmail.com>
2022-01-25 14:47:55 -0300

    structura de post usuario agregada

--- src/main/java/com/cliente/dr/controllers/UsuarioController.java
+++ src/main/java/com/cliente/dr/controllers/UsuarioController.java
@@ -4,10 +4,7 @@ package com.cliente.dr.controllers;
 import com.cliente.dr.dao.UsuarioDao;
 import com.cliente.dr.models.Usuario;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,13 +14,18 @@ public class UsuarioController {
     private UsuarioDao usuarioDao;
 
     @RequestMapping(value = "api/usuarios")
-    public List<Usuario> getUsuarios(){
+    public List<Usuario> getUsuarios() {
         return usuarioDao.getUsuarios();
     }
 
     @RequestMapping(value = "api/usuarios/{id}", method = RequestMethod.DELETE)
-    public void deleteUsuarios(@PathVariable long id){
+    public void deleteUsuarios(@PathVariable long id) {
         usuarioDao.deleteUsuario(id);
+    }
+
+    @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
+    public void postUsuarios(@RequestBody Usuario usuario){
+        usuarioDao.postUsuario(usuario);
     }
 
 
